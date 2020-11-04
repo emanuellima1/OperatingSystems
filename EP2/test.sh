1f6fd1beccbf0b6e0f7c66fa16a3736da322f4cc
João Seckler <jseckler@riseup.net>
2020-11-04 10:39:45 -0300

    EP2: test.sh: launch one instance of ep2 at a time
    Signed-off-by: João Seckler <jseckler@riseup.net>

--- EP2/test.sh
+++ EP2/test.sh
@@ -4,10 +4,7 @@
 rm -f time.out mem.out
 for i in {1..30}; do
     /usr/bin/time -p ./ep2 $1 $2 2>&1 > /dev/null | tail -3 | head -1 | awk '{print $2}'a >> time.out &
-    pids[${i}]=$!
     pmap $! | tail -1 | grep -P -o '\d*(?=K)' >> mem.out
+    wait $!
 done
 
-for pid in ${pids[*]}; do
-    wait $pid
-done
